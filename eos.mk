2911c24984949208def1724e52ec8208d5756749
Dan Pasanen <dan.pasanen@gmail.com>
2015-09-04 08:50:23 +0200

    peregrine: Specify GSM Client id
    Change-Id: Ibc3de767c7c9ccd5ed3a2477a71ccea7c1e80cf9

--- eos.mk
+++ eos.mk
@@ -7,6 +7,8 @@ $(call inherit-product, vendor/eos/config/common_full_phone.mk)
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := eos_peregrine
 
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
 # Copy device specific prebuilt files.
 PRODUCT_COPY_FILES += \
    device/motorola/msm8226-common/configs/bootanimation.zip:system/media/bootanimation.zip
